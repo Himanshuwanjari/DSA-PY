7a2bc6813d90b88ed8c08bacb0a37b370d8735d6
Himanshuwanjari <himanshuwanjari16@gmail.com>
2026-05-18 12:10:24 +0530

    insertion sort added

---
+++ day6/practice.py
@@ -0,0 +1,46 @@
+# input="Hello world"
+# mylist=input.split(" ")
+# # print(mylist)
+# ans=""
+# for ele in mylist:
+#     rev=""
+#     for i in ele:
+#         rev=i+rev
+#     ans+=rev+" "
+# print(ans)
+
+
+# input="({{()}})"
+# # output=valid
+# ans=0
+# myList=[]
+# for i in input:
+#     if i=='(' or i=='{' or i=='[':
+#         myList.append(i)
+#     elif i==')': 
+#         if '(' in myList:
+#             myList.remove('(')
+#         else:
+#             myList.append(i)
+#     elif i=='}': 
+#         if '{' in myList:
+#             myList.remove('{')
+#         else:
+#             myList.append(i)
+#     elif i==']': 
+#         if '[' in myList:
+#             myList.remove('[')
+#         else:
+#             myList.append(i)
+#     else:
+#         print('Invalid')
+    
+# if myList==[]:
+#     print('Valid')
+# else:
+#     print('Invalid')
+
+
+
+
+
