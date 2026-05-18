771c6761f6fcfdb61bde467186450c8265efe720
Himanshuwanjari <himanshuwanjari16@gmail.com>
2026-05-18 16:58:40 +0530

    day-6

--- day6/practice.py
+++ day6/practice.py
@@ -41,6 +41,40 @@
 #     print('Invalid')
 
 
+# myList=[4,3,2,7,8,2,1,5,5]
+# dic={}
+# for ele in myList:
+#     if ele not in dic:
+#         dic[ele]=0
+#     dic[ele]+=1
+# # print(dic)
+# for ele in dic:
+#     if dic[ele] > 1:
+#         print(ele,end=" ")
 
 
+# ----------------------------------------------------------------
+# dic={'C':3,'B':2,'A':1}
+# asc={}
+# while dic !={}:
+#     if dic[i]
+# ----------------------------------------------------------------
 
+class College:
+    collegename="Modern College"  # static variable
+
+    def __init__(self):
+            self.studentname="Prashant"
+
+principal=College()
+teacher=College()
+accountant=College()
+
+print("principal= " ,principal.collegename, ".........", principal.studentname)
+print("teacher= ", teacher.collegename, ".......", teacher.studentname)
+print("accountant= ", accountant.collegename, ".........", accountant.studentname)
+College.collegename='HBD'
+principal.studentname="Prashant jha"
+print("principal= " ,principal.collegename, ".........", principal.studentname)
+print("teacher= ", teacher.collegename, ".......", teacher.studentname)
+print("accountant= ", accountant.collegename, ".........", accountant.studentname)
